878712da156d56a4ae640188c601d736be80f8d9
Salman Younus <sam.awesome78666@gmail.com>
2021-09-24 01:26:12 +0500

    draw line with color added

--- api_salman.py
+++ api_salman.py
@@ -30,7 +30,35 @@ def draw_line_dda(img: MyImage, P: (int, int), Q: (int, int)):
     img.putpixel(Q, (255, 255, 255, 255))
 
 
+def draw_line(img: MyImage, P: (int, int), Q: (int, int), P_color: (int,)*4, Q_color: (int,)*4):
+    img.putpixel(P, (255, 255, 255, 255))
+    
+    gradient_x, gradient_y, max_dist = calc_gradient(P, Q)
 
+    current_pos = P
+    actual_x, actual_y = current_pos
+    
+    current_color = list(P_color)
+    actual_color = current_color
+
+
+    gradient_color = []
+    for i in range(4):
+        gradient_color.append((Q_color[i] - P_color[i]) / max_dist)
+    
+    for _ in range(max_dist):
+        actual_x += gradient_x
+        actual_y += gradient_y
+        
+        current_pos = (round(actual_x), round(actual_y))
+        
+        for i in range(4):
+            actual_color[i] += gradient_color[i]
+            current_color[i] = round(actual_color[i])
+        
+        img.putpixel(current_pos, tuple(current_color))
+
+    img.putpixel(Q, (255, 255, 255, 255))
 
 
 # Size of image
@@ -40,10 +68,14 @@ image_size = (20, 10)
 P = (0, 4)
 Q = (9, 0)
  
+# Color of points
+P_color = (255, 0, 0, 255)
+Q_color = (0, 0, 255, 255)
 
 img = MyImage(image_size, 3, 10, 'RGBA')
 
-draw_line_dda(img, P, Q)
+# draw_line_dda(img, P, Q)
+draw_line(img, P, Q, P_color, Q_color)
 
-# img.show()
+img.show()
 img.img.save("draw_line_dda.png")
