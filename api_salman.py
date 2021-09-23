5d381c8ebeef27cf93146a97ae49227c90c19ff3
Salman Younus <sam.awesome78666@gmail.com>
2021-09-24 00:45:16 +0500

    Fixed DDA Algo

--- api_salman.py
+++ api_salman.py
@@ -1,44 +1,49 @@
 from myimage import *
 import math, numpy
 
+def calc_gradient(P: (int, int), Q: (int, int)):
+    dy = Q[1] - P[1]
+    dx = Q[0] - P[0]
+
+    max_dist = max(abs(dy), abs(dx))
+
+    dx /= max_dist
+    dy /= max_dist
+
+    return dx, dy, max_dist
+
 def draw_line_dda(img: MyImage, P: (int, int), Q: (int, int)):
     img.putpixel(P, (255, 255, 255, 255))
+    
+    gradient_x, gradient_y, max_dist = calc_gradient(P, Q)
 
-    if (P[0] == Q[0]):
-        gradient = Q[1] - P[1]
-        offset = 0
-    else:
-        gradient = (Q[1] - P[1])/(Q[0] - P[0])
-        offset = 1 if (Q[0] - P[0]) >= 1 else -1
+    current_pos = P
+    actual_x, actual_y = current_pos
 
-    if Q[0] < P[0]: gradient *= -1
+    for _ in range(max_dist):
+        actual_x += gradient_x
+        actual_y += gradient_y
 
-    current_pos = list(P)
-    actual_y = current_pos[1]
-
-
-    while current_pos != list(Q):
-        print(current_pos, actual_y)
-        current_pos[0] += offset
-        actual_y += min(1, gradient)
-        current_pos[1] = round(actual_y)
-
+        current_pos = (round(actual_x), round(actual_y))
         img.putpixel(current_pos, (255, 0, 0, 255))
 
     img.putpixel(Q, (255, 255, 255, 255))
-    
+
+
+
+
 
 # Size of image
 image_size = (20, 10)
 
 # Starting and Ending points
-P = (0, 1)
-Q = (8, 0)
+P = (0, 4)
+Q = (9, 0)
  
 
 img = MyImage(image_size, 3, 10, 'RGBA')
 
 draw_line_dda(img, P, Q)
 
-img.show()
+# img.show()
 img.img.save("draw_line_dda.png")
