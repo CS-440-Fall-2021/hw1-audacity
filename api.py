96794a11469c9b069e385dddb41a5c2ddaef20ea
Anand Kumar <ak05173@st.habib.edu.pk>
2021-09-28 21:54:08 +0500

    Fixes

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from myimage import *
-import math, numpy
+import math
+import numpy
 
 # Calculate gradients of line
+
+
 def calc_gradient(P: (int, int), Q: (int, int)):
     dy = Q[1] - P[1]
     dx = Q[0] - P[0]
@@ -14,11 +17,14 @@ def calc_gradient(P: (int, int), Q: (int, int)):
     return dx, dy, max_dist
 
 # Draws a line using the DDA Algo
+
+
 def draw_line_dda(img: MyImage, P: (int, int), Q: (int, int)):
-    if (P == Q): return
-    
+    if (P == Q):
+        return
+
     img.putpixel(P, (255, 255, 255, 255))
-    
+
     gradient_x, gradient_y, max_dist = calc_gradient(P, Q)
 
     current_pos = P
@@ -36,21 +42,22 @@ def draw_line_dda(img: MyImage, P: (int, int), Q: (int, int)):
 
 
 # Draws a line with a color gradient
-def draw_line(img: MyImage, P: (int, int), Q: (int, int), \
-                P_color: (int,)*4, Q_color: (int,)*4) -> ( (int,)*3, (int,)*4 ):
+def draw_line(img: MyImage, P: (int, int), Q: (int, int),
+              P_color: (int,)*4, Q_color: (int,)*4) -> ((int,)*3, (int,)*4):
 
-    point_color = [] # Stores (coordinates, color) pairs of pixels in line
+    point_color = []  # Stores (coordinates, color) pairs of pixels in line
 
     img.putpixel(P, P_color)
     point_color.append((P, P_color))
-    
-    if (P == Q): return []
+
+    if (P == Q):
+        return []
 
     gradient_x, gradient_y, max_dist = calc_gradient(P, Q)
 
     current_pos = P
     actual_x, actual_y = current_pos
-    
+
     current_color = list(P_color)[:]
     actual_color = current_color[:]
 
@@ -58,18 +65,17 @@ def draw_line(img: MyImage, P: (int, int), Q: (int, int), \
     gradient_color = []
     for i in range(4):
         gradient_color.append((Q_color[i] - P_color[i]) / (max_dist+1))
-    
 
     # Updates x, y coordinates and color channels to plot the line
     for _ in range(max_dist+1):
         actual_x += gradient_x
         actual_y += gradient_y
-        
+
         current_pos = (round(actual_x), round(actual_y))
-        
+
         for i in range(4):
-            actual_color[i] += gradient_color[i]            
-            
+            actual_color[i] += gradient_color[i]
+
             current_color[i] = round(actual_color[i])
 
         img.putpixel(current_pos, (tuple(current_color)))
@@ -81,33 +87,39 @@ def draw_line(img: MyImage, P: (int, int), Q: (int, int), \
     return point_color
 
 # Draws a polygon by connecting successive points
+
+
 def draw_polygon_dda(img: MyImage, points: [(int, int), ...], colors: [(int, int, int, int), ...]):
 
     for i in range(1, len(points)):
-        draw_line(img, P = points[i-1], Q = points[i], P_color = colors[i-1], Q_color = colors[i])
+        draw_line(img, P=points[i-1], Q=points[i],
+                  P_color=colors[i-1], Q_color=colors[i])
 
     if len(points) > 2:
-        draw_line(img, P = points[0], Q = points[-1], P_color = colors[0], Q_color = colors[-1])
+        draw_line(img, P=points[0], Q=points[-1],
+                  P_color=colors[0], Q_color=colors[-1])
     else:
         img.putpixel(points[0], colors[0])
 
 
 # Draws a polygon by connecting successive points and fills it
 def draw_polygon(img: MyImage, points: [(int, int), ...], colors: [(int, int, int, int), ...], fill: bool = True):
-    point_color = [] # Stores (coordinates, color) pairs
+    point_color = []  # Stores (coordinates, color) pairs
 
     for i in range(1, len(points)):
-        point_color += draw_line(img, P = points[i-1], Q = points[i], P_color = colors[i-1], Q_color = colors[i])
+        point_color += draw_line(img, P=points[i-1], Q=points[i],
+                                 P_color=colors[i-1], Q_color=colors[i])
 
     if len(points) > 2:
-        point_color += draw_line(img, P = points[0], Q = points[-1], P_color = colors[0], Q_color = colors[-1])
+        point_color += draw_line(img, P=points[0], Q=points[-1],
+                                 P_color=colors[0], Q_color=colors[-1])
     else:
         img.putpixel(points[0], colors[0])
-    
+
     lines_drawn = 0
 
     if fill:
-        row_separated = {} # Separates each row for filling
+        row_separated = {}  # Separates each row for filling
 
         for i in point_color:
             if i[0][1] not in row_separated:
@@ -117,30 +129,28 @@ def draw_polygon(img: MyImage, points: [(int, int), ...], colors: [(int, int, in
         for row in row_separated:
             row_ = list(row_separated[row].items())
             row_.sort(key=lambda x: x[0])
-            
+
             for k in range(1, len(row_)):
-                draw_line(img, P = (row_[k-1][0], row), Q = (row_[k][0], row), P_color = row_[k-1][1], Q_color = row_[k][1])
+                draw_line(img, P=(row_[k-1][0], row), Q=(row_[k][0],
+                          row), P_color=row_[k-1][1], Q_color=row_[k][1])
                 lines_drawn += 1
-
-
 
 
 def api_test():
     # Size of image
     image_size_line = (20, 10)
-    image_size_polygon = (200, 200)
-
+    image_size_polygon = (110, 110)
 
     # Starting and Ending points for draw_line
     P = (0, 4)
     Q = (9, 0)
-    
+
     # Color of points
     P_color = (255, 0, 0, 255)
     Q_color = (0, 0, 255, 255)
 
     # List of points for polygon
-    points = [(50, 100), (100, 0), (150, 100)]
+    points = [(0, 100), (50, 0), (100, 100)]
     # points = [(1, 1), (1, 9), (18, 9), (18, 1)]
     # points = [(1, 8), (10, 1), (18, 8)]
     # points = [(5,5), (10, 9)]
@@ -151,16 +161,15 @@ def api_test():
     # colors = [(255, 0, 0, 255), (0, 255, 0 ,255), (0, 0, 255, 255)]
     # colors = [(255, 0, 0, 255), (0, 255, 0, 255)]
 
-
     img_line = MyImage(image_size_line, 3, 10, 'RGBA')
     img_polygon = MyImage(image_size_polygon, 3, 10, 'RGBA')
-
 
     draw_line(img_line, P, Q, P_color, Q_color)
     draw_polygon(img_polygon, points, colors, True)
 
     img_line.show()
     img_polygon.show()
+
 
 if __name__ == '__main__':
     api_test()
--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from api import *
 
+
 def getMid(p1, p2):  # calculate mid point of a line between two points.
     return ((p1[0]+p2[0]) // 2, (p1[1] + p2[1]) // 2)
 
@@ -7,16 +8,16 @@ def getMid(p1, p2):  # calculate mid point of a line between two points.
 def calc_mid(p1, p2, origin):  # used to calculate the point on the arc of the circle
     # calculate mid point of line between p1 and p2
     mid_point = getMid(p1, p2)
-    
+
     # generate vector from mid to center of circle
     vector = (mid_point[0] - origin[0], mid_point[1] - origin[1])
-    
+
     norm = math.sqrt(vector[0]**2 + vector[1]**2)  # norm of the vector
     unit_vec = [vector[0]/norm, vector[1]/norm]  # turn into unit vector
-    
+
     new_point = (int(origin[0] + origin[0]*unit_vec[0]),  # generate new point by scaling vector to r and adding center
                  int(origin[1] + origin[0]*unit_vec[1]))
-    
+
     return new_point
 
 
@@ -25,49 +26,49 @@ def circle(radius, subdivs=0):
     img = MyImage((radius*2*subdivs + 1, radius*2 + 1), 0, 1)
 
     img.fill((255, 255, 255, 255))  # fill the image with white background
-    
+
     points = [(radius, 0), (0, radius), (radius, radius*2),
               (radius*2, radius)]  # initial points for diamond
-    
+
     colors = [(255, 0, 0, 255), (255, 0, 0, 255),
               (255, 0, 0, 255), (255, 0, 0, 255)]  # initial colors for diamond
-    
+
     draw_polygon_dda(img, points, colors)  # draw diamond
-    
+
     for i in range(1, subdivs):  # loop over subdivs
-        
+
         # array to store initail points + midpoints on the arc of circle (new points)
         npoints = []
-        
+
         for j in range(len(points) - 1):  # loop over all points except last
             mid = calc_mid(
                 points[j], points[j+1], (radius, radius))  # midpoint on the arc
-            
+
             npoints.append(points[j])
             npoints.append(mid)
             npoints.append(points[j+1])
-        
+
         # calculating mid from last to first point
         mid = calc_mid(
             points[-1], points[0], (radius, radius))
-        
+
         npoints.append(points[-1])
         npoints.append(mid)
         npoints.append(points[0])
 
         # adjusted points to draw polygon after the old one (printing points)
         ppoints = []
-        
+
         for x in range(len(npoints)):
             # adding radius*2 to adjust for new subdiv
             ppoints.append((npoints[x][0] + (radius*2*i), npoints[x][1]))
-        
+
         for z in range(len(npoints) - len(colors)):  # compensating for colors
             colors.append((255, 0, 0, 255))
-        
+
         draw_polygon_dda(img, ppoints, colors)  # drawing new polygon
         points = npoints  # changeing old points to new points
-    
+
     return img
 
 
@@ -75,50 +76,51 @@ def sierpinski_level(img, level, points):  # recursive funtion to draw triangles
     if level == 1:  # only one triangle
         draw_polygon(img, points, ((0, 0, 0, 255),  # draw triangle
                      (0, 0, 0, 255), (0, 0, 0, 255)))
-    
+
     else:
         # get mid points on the edges of the triangle
         mid1, mid2, mid3 = getMid(points[0], points[1]), getMid(
-            points[1], points[2]), getMid(points[0], points[2])  
-        
+            points[1], points[2]), getMid(points[0], points[2])
+
         # draw triangles using mid points
         sierpinski_level(img, level - 1, (points[0], mid1, mid3))
         sierpinski_level(img, level - 1, (mid1, points[1], mid2))
         sierpinski_level(img, level - 1, (mid3, mid2, points[2]))
-    
+
     return img
 
 
 def sierpinski(level):  # main function
 
     # dimension for each triangle (reduce it speed up the process, reducing will reduce the quality too)
-    dimension = 500
-    
+    dimension = 300
+
     gap = 10  # gap between each triangle in pixels
-    
+
     level += 1
     img = MyImage(((dimension*level) + (gap*level) + 1, dimension + 1), 0, 10)
     img.fill((255, 255, 255, 255))  # fill with white background
-    
+
     # initial points for first triangle
     points = [(0, dimension), (dimension//2, 0), (dimension, dimension)]
-    
+
     for i in range(1, level):
         sierpinski_level(img, i, points)  # call recursive function
-        
+
         # adjust for next triangle
         points[0] = points[0][0] + dimension + gap, points[0][1]
         points[1] = points[1][0] + dimension + gap, points[1][1]
         points[2] = points[2][0] + dimension + gap, points[2][1]
-    
+
     img = sierpinski_level(img, level, points)
-    
+
     return img
 
 
 def application_test():  # funct to test circle and sierpinski
     circle(100, 4).show()
     sierpinski(4).show()
+
 
 if __name__ == '__main__':
     application_test()
